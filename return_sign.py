754acf68d6c00398f81f5ee21a35a64bf51af888
Suhrid Singh <suhrids88@gmail.com>
2025-09-21 19:38:54 +0530

    Create return_sign.py

---
+++ return_sign.py
@@ -0,0 +1,11 @@
+# Your code goes here. Define a function called 'sign'
+def sign(num):
+    if num<0:
+        return -1
+    elif num==0:
+        return 0
+    else:
+        return 1
+     
+# Check your answer
+q1.check()
