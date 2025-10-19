139422988432b8af08c379a88866a89b8d3f0bb6
Suhrid Singh <suhrids88@gmail.com>
2025-10-19 15:13:43 +0530

    Create check_pincode.py

---
+++ check_pincode.py
@@ -0,0 +1,7 @@
+def is_valid_zip(zip_code):
+    """Returns whether the input string is a valid (5 digit) zip code
+    """
+    if len(zip_code)==5 and zip_code.isdigit():
+        return True
+    else:
+        return False
